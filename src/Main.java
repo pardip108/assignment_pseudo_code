7c6caa92a09ab1f9d62158082dc2310cda6ce5f6
pardip108 <pk620115@gmail.com>
2026-03-18 11:12:15 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        //q2) write pseudo code to swap two numbers using a temprory variable and without temp
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println("before swapping");
+        System.out.println("x = "+ x + " y = "+ y);
+        int temp = y;
+        y = x;
+        x = temp;
+        System.out.println("after swapping");
+        System.out.println("x = "+ x + " y = "+ y);
+
+        //without a temproray variable
+
+        x = x+y;
+        y = x-y;
+        x = x-y;
+        //or here can use xor operator to swap
+
+        */
+
+
+        /*
+        //q1 print x +y
+        int x = 3;
+        int y = 5;
+        //increment x
+        x++;
+        y = x+y;
+        y++;
+        x = y*2;
+        System.out.println("x+y: " + x+y);
+
+         */
+    }
+}
